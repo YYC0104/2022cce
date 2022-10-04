046f791ceb36cba7a8452c2adc5aed490bbcaddd
YYC0104 <112918713+YYC0104@users.noreply.github.com>
2022-10-04 11:01:06 +0800

    week05

---
+++ week05-2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int n=3;
+    while (n>0)
+    {
+        printf("n¬O%i, n>0\n", n);
+        n--;
+    }
+}
---
+++ week05-3.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    for(int n=3; n>0; n--)
+    {
+        printf("n¬O%d, n>0\n", n);
+    }
+}
